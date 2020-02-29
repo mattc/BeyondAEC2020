d6da0fcbff4453407bf6ac4b4e5115722558bc4b
ndloughrey <30737512+ndloughrey@users.noreply.github.com>
2020-02-29 00:53:03 -0500

    Parameter driven rooms with department labels
    Added variables to calculate "percentage" of rooms within main shelving space.  Added department labels to rooms.

--- SectionLayout/src/SectionLayout.cs
+++ SectionLayout/src/SectionLayout.cs
@@ -23,29 +23,68 @@ namespace SectionLayout
             var output = new SectionLayoutOutputs(volume);
             var rectangle = Polygon.Rectangle(input.Length, input.Width);
 
+            //Define Dimensions of non-product spaces
+            var entryDepth = 5;
+            var checkoutDepth = 10;
+            var serviceDepth = 10;
+
+            //Variables driving the division of the main shelf space
+            var _percentProduce = input.PercentProduce;
+            var _percentGeneral = input.PercentGeneral;
+            var _percentRefrigerated = input.PercentRefrigerated;
+
+            var totalShelf = _percentProduce + _percentGeneral + _percentRefrigerated;
+
+            var percentProduce = _percentProduce / totalShelf;
+            var percentGeneral = _percentGeneral / totalShelf;
+            var percentRefrigerated = _percentRefrigerated / totalShelf;
+            
+            //Create room representing entire store
             var material = new Material("office",Colors.Aqua);
             var solid = new Elements.Geometry.Solids.Extrude(rectangle, height, Vector3.ZAxis, false);
             var geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>(){ solid});
              Room rm = new Room(rectangle, Vector3.ZAxis, "Section 1", "100", "Grocery", "100", rectangle.Area(), 
             1.0, 0, 0, height, rectangle.Area(), new Transform(), material, geomRep, false, System.Guid.NewGuid(), "Section 1" );
 
+            //Split into rooms front to back
             var grid = new Grid2d(rectangle);
-            grid.U.DivideByFixedLength(3, FixedDivisionMode.RemainderAtBothEnds);
-            grid.V.DivideByPattern(new[] { 1.0, 5.0 });
-            var rooms = grid.GetCells().Select(c => GetRoomFromCell(c));
-            output.model.AddElements(rooms);
+            grid.V.SplitAtParameters(new[] {entryDepth/input.Length, checkoutDepth/input.Length, (1 - serviceDepth/input.Length)});
             
-            output.model.AddElement(rm);
+            var entryArea = grid.GetCellAtIndices(0,0);
+            var checkoutArea = grid.GetCellAtIndices(0,1);
+            var shelfArea = grid.GetCellAtIndices(0,2);
+            var serviceArea = grid.GetCellAtIndices(0,3);
+            
+            //Split Shelf Area into sub-rooms
+            shelfArea.U.SplitAtParameters(new[] {percentProduce, percentGeneral});
+            var produce = shelfArea.GetCellAtIndices(0,0);
+            var general = shelfArea.GetCellAtIndices(1,0);
+            var refrig = shelfArea.GetCellAtIndices(2,0);
+
+            //var rooms = grid.GetCells().Select(c => GetRoomFromCell(c));
+            //output.model.AddElements(rooms);
+            
+            //Label and return rooms --> shelf area excluded due to inclusion of sub-rooms
+            output.model.AddElement(GetRoomFromCell(entryArea, "entry"));
+            output.model.AddElement(GetRoomFromCell(checkoutArea, "checkout"));
+            ////output.model.AddElement(GetRoomFromCell(shelfArea, "shelf"));
+            output.model.AddElement(GetRoomFromCell(serviceArea, "service"));
+
+            output.model.AddElement(GetRoomFromCell(produce, "produce"));
+            output.model.AddElement(GetRoomFromCell(general, "general"));
+            output.model.AddElement(GetRoomFromCell(refrig, "refrig"));
+            
+            ////output.model.AddElement(rm);
             return output;
         }
 
-        private static Element GetRoomFromCell (Grid2d cell)
+        private static Element GetRoomFromCell (Grid2d cell, string department)
         {
             var polygon = (Polygon)cell.GetCellGeometry();
-            var material = new Material("office",Colors.Red);
+            var material = new Material("office",new Color(1,0,0,.9));
             var solid = new Elements.Geometry.Solids.Extrude(polygon, 11, Vector3.ZAxis, false);
             var geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>(){ solid});
-            var room = new Room((Polygon)polygon, Vector3.ZAxis, "Section 1", "100", "Grocery", "100", polygon.Area(), 
+            var room = new Room((Polygon)polygon, Vector3.ZAxis, "Section 1", "100", department, "100", polygon.Area(), 
             1.0, 0, 0, 11, polygon.Area(), new Transform(), material, geomRep, false, System.Guid.NewGuid(), "Section 1" );
             return room;
         }
